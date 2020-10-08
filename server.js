1905a99815315256688cfbd61c588322b6714b00
Linta Paul <54324010+LintaPaul@users.noreply.github.com>
2020-10-08 15:00:29 +0530

    addedclient

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://adminroot:admin1107@cluster-base.frs5y.mongodb.net/users?retryWrites=true&w=majority";
+const uri = "mongodb+srv://adminroot:admin1107@cluster-base.frs5y.mongodb.net/users?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
