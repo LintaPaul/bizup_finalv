5753c6d7689a7f6153a87eb20931660cf126ef91
LintaPaul <lintapaultkr1107@gmail.com>
2020-10-28 16:07:49 +0530

    friendsupdate

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,11 +1,59 @@
 <template>
   <div class="container">
     <NLink to="/feeds">Back home</NLink>
+    <div v-for="user_alias in User">
+                      <div v-show="user_alias._id===id">
+                        <div v-for="uf in user_alias.friends">
+                                             <h5>{{uf}}</h5>
+                                             </div>
+                      </div>
+    </div>
+    <div><br><br>
+                   <label><span>&nbsp;</span><input type="submit" v-on:click.prevent="generateprofile()" value="Submit" />
+                  </label>
+          </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Headers from "../components/Header.vue";
+import axios from 'axios';
+import {Router} from "vue-router";
+export default {
+  components: {
+    Headers,
+  },
+   name: 'use5',
+    data(){
+        return{
+            User: [],
+            id:this.$route.query.id
+            
+        }
+    },
+methods:{
+ /* mounted()
+{
+    axios.get('http://localhost:3000/api/users').then((response)=>{
+    console.log(response.data);
+    this.User = response.data;
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+},*/
+  generateprofile:function(id)
+  {
+    axios.get('http://localhost:3000/api/users?id='+ id).then((response)=>{
+    console.log(response.data);
+    this.User = response.data;
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+  }
+  }
+};
 </script>
 
 <style scoped>
